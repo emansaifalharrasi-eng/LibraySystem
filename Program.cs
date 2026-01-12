4aaf19390d956a2aaff6ade3c6f778db0bee6b7f
pc1 <pc1>
2026-01-12 08:07:40 +0400

    modify case1 ,case2

--- Program.cs
+++ Program.cs
@@ -19,17 +19,18 @@ namespace LibraySystem
             int LastbookIndextracker = -1;
 
             //seed data
-            booktitles[0] = "Ahmed";
-            bookISBNs[0] = "1568";
-            borrowernames[0] = "77";
+            booktitles[0] = "math";
+            bookISBNs[0] = "1";
+            borrowernames[0] = " ";
+            availability[0] = true;
             LastbookIndextracker++;
 
 
-            booktitles[1] = "Ali";
-            bookISBNs[1] = "1346";
-            borrowernames[1] = "50";
+            booktitles[1] = "cats";
+            bookISBNs[1] = "2";
+            borrowernames[1] = "asma";
             LastbookIndextracker++;
-
+            availability[1] = false; ;
 
 
             bool exit = false;
@@ -57,72 +58,81 @@ namespace LibraySystem
                 {
                     case 1:
 
-                 
+
                         Console.Write("Enter the book title ");
 
                         booktitles[LastbookIndextracker] = Console.ReadLine();
 
                         Console.Write("Enter the author name ");
-                        bookauthors[LastbookIndextracker ] = Console.ReadLine();
+                        bookauthors[LastbookIndextracker] = Console.ReadLine();
 
                         Console.Write("Enter the ISBN");
-                        bookISBNs [LastbookIndextracker] = Console.ReadLine();
+                        bookISBNs[LastbookIndextracker] = Console.ReadLine();
 
-                        bookISBNs[LastbookIndextracker ] = "id" +LastbookIndextracker + 1;
+                        bookISBNs[LastbookIndextracker] = "id" + LastbookIndextracker + 1;
 
 
 
 
                         Console.WriteLine("The new book is Added sucssfully");
-                        Console.WriteLine("ISBN is: " + bookISBNs[LastbookIndextracker ]);
+                        Console.WriteLine("ISBN is: " + bookISBNs[LastbookIndextracker]);
 
-                            LastbookIndextracker++ ;
+                        LastbookIndextracker++;
 
 
                         break;
 
-                    case 2:
-
-
-
-                        Console.Write("Enter the borrower name ");
-                        string borrowername = (Console.ReadLine());
+                    case 2:   
 
                         Console.Write("Enter the book title or the ISBN ");
-                        string keybook =(Console.ReadLine());
+                        string keybook = (Console.ReadLine());
 
 
                         bool bookfounded = false;
 
                         for (int i = 0; i < 100; i++)
                         {
-                            if (keybook == booktitles[i] || keybook ==booktitles[i])
+                            if (keybook == booktitles[i] || keybook == booktitles[i])
                             {
                                 borrowernames[i] = borrowername;
-                                availability[i] = false;
+                                
 
                                 bookfounded = true;
+                              if ( availability[i] == true)
+                                {
+                                    Console.Write("Enter the borrower name ");
+                                    string borrowername = (Console.ReadLine());
+                                     availability[i] = false ;
+                                    Console.WriteLine("Book borrowed succeful");
+                                    
 
-                                  
-
+                                    }
+                              else
+                                {
+                                    Console.WriteLine("book borrowed already");
                                 }
-
-
-
                                 break;
-                            
+
+
+                            }
+
+
+
+                            break;
+
 
                         }
 
-                        
+
                         if (bookfounded == false)
+
                         {
                             Console.WriteLine("sorry book not found");
                         }
                         else
                         {
                             Console.WriteLine(" the book is founded successful!");
-                           
+
 
                         }
 
@@ -133,7 +143,57 @@ namespace LibraySystem
                     case 3:
 
 
+                        Console.Write("Enter the book the ISBN ");
+                        string ISBNs = (Console.ReadLine());
+
+
+                        bool bookFounded = false;
+
+                        for (int i = 0; i < 100; i++)
+                        {
+                            if (ISBNs == bookISBNs[i])
+                            {
+
+
+
+                                bookfounded = true;
+                                if (availability[i] == false)
+                                {
+                                    Console.WriteLine("Enter the borrowname ");
+                                    string borrowname = (Console.ReadLine());
+                                    availability[i] = true;
+                                    Console.WriteLine(" the book is returned successful!");
+
+                                }
+
+                                else
+                                {
+                                    Console.WriteLine("this book was no borrowed");
+                                }
+
+                                break;
+                            }
+
+                        }
+                      if(bookFounded == false)
+                        {
+                            Console.WriteLine("sorry Book not found");
+                        }
                         break;
+                       
+
+                            
+
+                        
+                            
+
+
+
+                        
+
+
+
+                 
 
                     case 4:
 
